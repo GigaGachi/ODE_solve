d2b63d0a92a3835b3392bc2667df91031c95fbfb
GigaGachi <ustimovi@bk.ru>
2023-04-25 19:43:48 +0300

    v.1

--- differential.py
+++ differential.py
@@ -50,30 +50,28 @@ def ODE_solve_ERK4(u0,M,t0,tend,f):
     return u,t
 def f_to_len(u,t,f):
     dot = np.dot(f(u,t),f(u,t))
-    vec = func(u,t)/(1+dot)**0.5
+    vec = func(u,t)/(2+dot)**0.5
     return vec
 def t_to_len(u,t,f):
     dot = np.dot(f(u,t),f(u,t))
-    vec = 1/(1+dot)**0.5
+    vec = 1/(2+dot)**0.5
     return vec
-def ODE_solve_ERK4_qu(u0,M,t0,tend,f,tau):
+def ODE_solve_ERK4_qu(u0,t0,tend,f,tau):
     #схема эйлера
     t = []
     t.append(t0)
     shape_ = len(u0)
     u = []
     u.append(u0)
-    i=0
-    while t[i]<tend:
-        w1 = f_to_len(u[i],t[i],f)
-        w2 = f_to_len(u[i]+tau*w1/2,t[i]+tau/2)
-        w3 = f_to_len(u[i]+tau*w2/2,t[i]+tau/2)
-        w4 = f_to_len(u[i]+tau*w3,t[i]+tau)
-        tw1 = t_to_len(u[i],t[i],f)
-        tw2 = t_to_len(u[i]+tau*tw1/2,t[i]+tau/2)
-        tw3 = t_to_len(u[i]+tau*tw2/2,t[i]+tau/2)
-        tw4 = t_to_len(u[i]+tau*tw3,t[i]+tau)
-        t[i+1] = t[i] + tau*(tw1+2*tw2+2*tw3+tw4)/6
-        u[i+1] = u[i] + tau*(w1+2*w2+2*w3+w4)/6
-        i = i + 1
+    while t[-1]<tend:
+        w1 = f_to_len(u[-1],t[-1],f)
+        w2 = f_to_len(u[-1]+tau*w1/2,t[-1]+tau/2,f)
+        w3 = f_to_len(u[-1]+tau*w2/2,t[-1]+tau/2,f)
+        w4 = f_to_len(u[-1]+tau*w3,t[-1]+tau,f)
+        tw1 = t_to_len(u[-1],t[-1],f)
+        tw2 = t_to_len(u[-1]+tau*tw1/2,t[-1]+tau/2,f)
+        tw3 = t_to_len(u[-1]+tau*tw2/2,t[-1]+tau/2,f)
+        tw4 = t_to_len(u[-1]+tau*tw3,t[-1]+tau,f)
+        t.append(t[-1] + tau*(tw1+2*tw2+2*tw3+tw4)/6)
+        u.append(u[-1] + tau*(w1+2*w2+2*w3+w4)/6)
     return u,t
