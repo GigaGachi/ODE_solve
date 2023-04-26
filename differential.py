699f7f3ca0e47b9480faf18eb735ed83a1f7e248
GigaGachi <ustimovi@bk.ru>
2023-04-26 11:47:46 +0300

    v.2

--- differential.py
+++ differential.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy 
-import math
+from math import sqrt
 def func(u,t):
     #функция правой части уравнения
     y = np.array([u[1],t*t*u[0]],dtype=np.float32)  
@@ -48,30 +48,41 @@ def ODE_solve_ERK4(u0,M,t0,tend,f):
         t[i+1] = t[i] + tau
         u[i+1] = u[i] + tau*(w1+2*w2+2*w3+w4)/6
     return u,t
-def f_to_len(u,t,f):
+def f_to_len(u,f):
+    #замена правой части дифура для перехода к длине дуги
+    t = u[-1]
     dot = np.dot(f(u,t),f(u,t))
-    vec = func(u,t)/(2+dot)**0.5
-    return vec
-def t_to_len(u,t,f):
-    dot = np.dot(f(u,t),f(u,t))
-    vec = 1/(2+dot)**0.5
-    return vec
-def ODE_solve_ERK4_qu(u0,t0,tend,f,tau):
-    #схема эйлера
-    t = []
-    t.append(t0)
+    vec = f(u,t)/sqrt(1+dot)
+    time = 1/sqrt(1+dot)
+    return np.concatenate((vec,[time]),0)
+def ODE_solve_ERK4_qu(u0,t0,tend,f,tau,Num_points):
+    #рунге-кутта 4 порядка с квазиравномерной сеткой
     shape_ = len(u0)
-    u = []
-    u.append(u0)
-    while t[-1]<tend:
-        w1 = f_to_len(u[-1],t[-1],f)
-        w2 = f_to_len(u[-1]+tau*w1/2,t[-1]+tau/2,f)
-        w3 = f_to_len(u[-1]+tau*w2/2,t[-1]+tau/2,f)
-        w4 = f_to_len(u[-1]+tau*w3,t[-1]+tau,f)
-        tw1 = t_to_len(u[-1],t[-1],f)
-        tw2 = t_to_len(u[-1]+tau*tw1/2,t[-1]+tau/2,f)
-        tw3 = t_to_len(u[-1]+tau*tw2/2,t[-1]+tau/2,f)
-        tw4 = t_to_len(u[-1]+tau*tw3,t[-1]+tau,f)
-        t.append(t[-1] + tau*(tw1+2*tw2+2*tw3+tw4)/6)
-        u.append(u[-1] + tau*(w1+2*w2+2*w3+w4)/6)
-    return u,t
+    u_0 =  np.concatenate((u0,[t0]),0)
+    u = np.zeros((Num_points,shape_+1),np.float32)
+    u[0] =u_0
+    i=0
+    while u[i][-1]<tend:
+        w1 = f_to_len(u[i],f)
+        w2 = f_to_len(u[i]+tau*w1/2,f)
+        w3 = f_to_len(u[i]+tau*w2/2,f)
+        w4 = f_to_len(u[i]+tau*w3,f)
+        u[i+1] = u[i] + tau*(w1+2*w2+2*w3+w4)/6
+        i = i+1
+    return u[0:i,0:shape_],u[0:i,-1]
+def ODE_solve_ERK2_qu(u0,t0,tend,f,tau,Num_points):
+    #рунге-кутта 2 порядка с квазиравномерной сеткой
+    shape_ = len(u0)
+    u_0 =  np.concatenate((u0,[t0]),0)
+    u = np.zeros((Num_points,shape_+1),np.float32)
+    u[0] =u_0
+    i=0
+    while u[i][-1]<tend:
+        w1 = f_to_len(u[i],f)
+        w2 = f_to_len(u[i]+tau*w1*(2/3),f)
+        u[i+1] = u[i] + tau*((1/4)*w1+(3/4)*w2)
+        i = i+1
+    return u[0:i,0:shape_],u[0:i,-1]
+def delta_l(y,t):
+    #возвращает массив длин дуг разности двух соседних точек
+    return [sqrt((y[i+1][0]-y[i][0])**2+(t[i+1]-t[i])**2) for i in range(len(t)-1)]
